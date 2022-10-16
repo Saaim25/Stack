126efd28af12a9337d833e63a3f538085fe0005f
Saaim25 <76088337+Saaim25@users.noreply.github.com>
2022-10-16 15:35:49 +0530

    Create queueLL.java

---
+++ queueLL.java
@@ -0,0 +1,71 @@
+public class Main
+{
+    static class Node{
+        int data;
+        Node next;
+        
+        Node(int data){
+            this.data = data;
+            next = null;
+        }
+    }
+    
+    static class queue{
+        static Node head = null;
+        static Node tail = null;
+        
+        
+        
+        public static boolean isEmpty(){
+            return head== null && tail== null;
+        }
+    
+        public static void add(int data){
+            Node newNode = new Node(data);
+            if(isEmpty()){
+                tail= head= newNode;
+                return;
+            }
+            tail.next = newNode;
+            tail = newNode;
+        }
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("Empty List");
+                return -1;
+            }
+            int  front = head.data;
+            if(head==tail){
+                tail=null;
+            }
+            
+            head = head.next;
+            return front;
+        }
+        public static int peak(){
+            if(isEmpty()){
+                System.out.println("Empty List");
+                
+                return -1;
+            }
+            int front = head.data;
+            return front;
+        }
+    }
+	public static void main(String[] args) {
+		queue q = new queue();
+		q.add(1);
+		q.add(2);
+		q.add(3);
+		q.add(4);
+		q.add(5);
+		
+		while(!q.isEmpty()){
+		    System.out.println(q.peak());
+		    q.remove();
+		}
+		
+		
+		
+	}
+}
